f20f591d496e112914f7835a25bb5dd28184fd42
Kulakov Ilia <kulakov@selectel.ru>
2026-01-13 13:18:58 +0300

    Run clang formater
    * Fix code style with clang-format;

--- hw2/example.cpp
+++ hw2/example.cpp
@@ -2,6 +2,4 @@
 
 #include <iostream>
 
-void example_function() {
-	std::cout << "Hello from example function!" << std::endl;
-}
+void example_function () { std::cout << "Hello from example function!" << std::endl; }
--- hw2/move_me_out.cpp
+++ hw2/move_me_out.cpp
@@ -1,5 +1,3 @@
 #include <iostream>
 
-void todo_move_me_out() {
-	std::cout << "This function should be located in the separated source file" << std::endl;
-}
+void todo_move_me_out () { std::cout << "This function should be located in the separated source file" << std::endl; }
